4603300dc4208ea90deef022323815e31506d926
Dipin-Rajput India <dipinrajput707@gmail.com>
2025-02-12 13:36:56 +0530

    [Dipin]feat: Add check daily wage functionality for employees (UC2)

--- employeeWageProblem.cpp
+++ employeeWageProblem.cpp
@@ -5,7 +5,7 @@ using namespace std;
 
 // UC-1: Check Attendance
 
-void checkAttendance(){
+int checkAttendance(){
 
     // srand(seed): function to generate different random number each time.
     // seed: is an integer used to intialize the random number generator.
@@ -21,13 +21,34 @@ void checkAttendance(){
     else{
         cout << "\nEmployee is Absent\n";
     }
+    
+    return attendance;
+}
+
+// UC-2: Check Daily Wage
+
+int checkDailyWage(){
+
+    int status = checkAttendance(); // Call check attendance function
+    int wagesPerHour = 20;
+    int fullDayHours = 8;
+    int wages = 0;
+
+    if(status == 1){
+        wages = wagesPerHour * fullDayHours; // Calculating daily wage
+        return wages;
+    }
+    else{
+        return 0;
+    }
 }
 
 int main(){
 
     cout << "\n-------------------- Welcome to Employee Wage Computation Program --------------------\n";
 
-    cout << "\nEnter 1 to check attendance\n";
+    cout << "\nEnter 1 to check attendance";
+    cout << "\nEnter 2 to check daily wage\n";
 
     int choice;
 
@@ -36,6 +57,10 @@ int main(){
 
     if(choice == 1){
         checkAttendance(); // function call to check whether employee is present or absent
+    }
+    else if(choice == 2){
+        int fullDayWage = checkDailyWage();
+        cout << "\nYour daily wage is: " << fullDayWage << "\n"; 
     }
     else{
         cout << "\nThank You for joining";
